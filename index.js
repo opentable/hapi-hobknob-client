d3d71639c89c8fedaa334b34f6e74b682ebeed82
Chris Ridde <criddle@opentable.com>
2014-09-09 11:39:31 +0100

    First commit of hobknob hapi plugin

---
+++ index.js
@@ -0,0 +1,23 @@
+exports.register = function(plugin, options, next) {
+  plugin.log(["hoknob-client"], "Initializing Hobknob client");
+
+  if (!options.client){
+    next(new Error("Options.client does not exist"));
+    return;
+  }
+
+  options.client.initialise(function(err){
+
+    if (err){
+      next(err);
+    }
+
+    plugin.log(["hoknob-client"], "Hobknob initialized");
+    next();
+
+  });
+};
+
+exports.register.attributes = {
+  pkg: require('./package.json')
+};
